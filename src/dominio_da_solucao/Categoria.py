aeeb94edaf48f15851d5a24b0874eca3b1c6c535
robertotxr <robertobeckerbt@gmail.com>
2024-12-06 15:12:04 -0300

    Adicao de auxiliares para poder fazer os diagramas

--- src/dominio_da_solucao/Categoria.py
+++ src/dominio_da_solucao/Categoria.py
@@ -1,81 +1,87 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from dominio_da_solucao import Tabela
-from dominio_da_solucao import Mesa
-from dominio_da_solucao import Prisao
+from dominio_da_solucao.Tabela import Tabela
 from typing import List
 
 class Categoria(object):
 
-	def atribuir_pontuacao(self, categoria_escolhida : int):
-		mesa = Mesa()
-		dados = mesa.get_dados()
+	def atribuir_pontuacao(self, categoria_escolhida, dados):
 
 		if categoria_escolhida in [1,2,3,4,5,6]:
-			Tabela.soma_dados(dados, categoria_escolhida)
+			return self.tabela.soma_dados(categoria_escolhida, dados)
 
 		if categoria_escolhida == 7: # four of a kind
-			if self.valida_for_of_a_kind():
-				Tabela.atribui_full_house()
+			if self.valida_for_of_a_kind(dados):
+				return self.tabela.atribui_four_of_a_kind()
 			else:
-				Tabela.atribui_zero()
+				return self.tabela.atribui_zero()
 
 		if categoria_escolhida == 8: # full house
-			if self.valida_full_house():
-				Tabela.atribui_full_house()
+			if self.valida_full_house(dados):
+				return self.tabela.atribui_full_house()
 			else:
-				Tabela.atribui_zero()
+				return self.tabela.atribui_zero()
 
 		if categoria_escolhida == 9: # small straight
-			if self.valida_small_straight():
-				Tabela.atribui_full_house()
+			if self.valida_small_straight(dados):
+				return self.tabela.atribui_small_straight()
 			else:
-				Tabela.atribui_zero()	
+				return self.tabela.atribui_zero()	
 
 		if categoria_escolhida == 10: # big straight
-			if self.valida_big_staight():
-				Tabela.atribui_full_house()
+			if self.valida_big_staight(dados):
+				return self.tabela.atribui_big_straight()
 			else:
-				Tabela.atribui_zero()
+				return self.tabela.atribui_zero()
 
 		if categoria_escolhida == 11: # yatch
-			if self.valida_yatch():
-				Tabela.atribui_full_house()
+			if self.valida_yatch(dados):
+				return self.tabela.atribui_yatch()
 			else:
-				Tabela.atribui_zero()
+				return self.tabela.atribui_zero()
 
 	def valida_for_of_a_kind(self, dados : list) -> int:
-		return any(dados.count(dado) >= 4 for dado in set(dados))
+		if any(dados.count(dado) >= 4 for dado in set(dados)):
+			aux_valido = True
+		else: aux_valido = False
+
+		return aux_valido
 
 	def valida_full_house(self, dados : list) -> int:
 		contagens = []
 		contagens = [dados.count(dado) for dado in set(dados)]
-		return sorted(contagens) == [2, 3]
+
+		if sorted(contagens) == [2, 3]:
+			aux_valido = True
+		else: aux_valido = False
+
+		return aux_valido
 
 	def valida_small_straight(self, dados : list) -> int:
-		sequencias = [{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}]
-		return any(seq.issubset(set(dados)) for seq in sequencias)
+		sequencias = [{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}]    
+
+		if any(seq.issubset(set(dados)) for seq in sequencias):
+			aux_valido = True
+		else: aux_valido = False
+
+		return aux_valido
 
 	def valida_big_staight(self, dados : list) -> int:
 		lista_ordenada = sorted(dados)
+		aux_valido = False
 		
 		if lista_ordenada == list(range(lista_ordenada[0], lista_ordenada[0] + 5)):
-			return True
-		
-		return False
+			aux_valido = True
+		else: aux_valido = False
+
+		return aux_valido
 
 	def valida_yatch(self, dados : list) -> int:
 		return all(numero == dados[0] for numero in dados)
 
-	def atualizar_categoria(self, aPontos : int):
-		# [TODO] - Colocar o numero na interface
-		pass
-
 	def __init__(self):
-		self.___categoria_escolhida : int = None
-		self.___categoty_points : int = None
-		self._unnamed_Tabela_ : Tabela = None
-		"""# @AssociationMultiplicity 1"""
-		self._unnamed_Mesa_ : Mesa = None
-		self._unnamed_Prisao_ : Prisao = None
+		self.categoria_escolhida : int = None
+		self.categoty_points : int = None
+		self.tabela = Tabela()
+
 

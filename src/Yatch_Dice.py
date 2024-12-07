094542a6cc38de82a808467ee87782411f4d717a
Dudolho <eduardobgrasel@gmail.com>
2024-12-07 14:37:16 -0300

    font

--- src/Yatch_Dice.py
+++ src/Yatch_Dice.py
@@ -1,7 +1,11 @@
 from PlayerInterface import PlayerInterface
 from PySide6.QtWidgets import QApplication
 import sys
+from PySide6.QtGui import QFont
+
 app = QApplication(sys.argv)
+font = QFont("Arial", 12)  # Usando uma fonte amplamente disponível
+app.setFont(font)
 if 'main_window' not in globals():  # Garantir que não criamos outra instância
     main_window = PlayerInterface()
     main_window.show()

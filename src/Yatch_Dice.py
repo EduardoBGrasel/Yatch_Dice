f77b3dcd5ca7d88a99eba5002d1ead428122086f
Dudolho <eduardobgrasel@gmail.com>
2024-12-08 10:03:27 -0300

    adicionado fonte na raiz do projeto

--- src/Yatch_Dice.py
+++ src/Yatch_Dice.py
@@ -1,10 +1,12 @@
 from PlayerInterface import PlayerInterface
 from PySide6.QtWidgets import QApplication
 import sys
-from PySide6.QtGui import QFont
+from PySide6.QtGui import QFontDatabase, QFont
 
 app = QApplication(sys.argv)
-font = QFont("Arial", 12)  # Usando uma fonte amplamente disponível
+font_id = QFontDatabase.addApplicationFont("font/Arial.ttf")  # Caminho do arquivo de fonte
+font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+font = QFont(font_family, 12)
 app.setFont(font)
 if 'main_window' not in globals():  # Garantir que não criamos outra instância
     main_window = PlayerInterface()
